1d2be599daefb906d7eab64f9c5990726ddbfcfb
Vahe Hakobyan <vahe.g.hakobyan@gmail.com>
2022-05-18 14:53:23 +0400

    Create empty project

---
+++ iMusicTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iMusicTask
+//
+//  Created by Vahe Hakobyan on 18.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
